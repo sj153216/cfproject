e2d9c9cf7c33ccd1095231560d99977837cc629b
sj <15005170827@163.com>
2016-07-12 07:56:31 +0800

    1.增加了启动界面 2.引入了RecyclerView

--- app/src/main/java/com/sjhcn/myfragment/SecondFragment.java
+++ app/src/main/java/com/sjhcn/myfragment/SecondFragment.java
@@ -2,6 +2,7 @@ package com.sjhcn.myfragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,15 @@ import com.sjhcn.qrcode.R;
  * Created by sjhcn on 2016/7/12.
  */
 public class SecondFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.second_fragment, container, false);
         TextView textView = (TextView) view.findViewById(R.id.tv);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.second_recycler_view);
 
         return view;
     }
---
+++ app/src/main/java/com/sjhcn/qrcode/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.sjhcn.qrcode;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by sjhcn on 2016/7/11.
+ */
+public class SplashActivity extends Activity {
+    private static final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+                overridePendingTransition(android.R.anim.cycle_interpolator, android.R.anim.bounce_interpolator);
+
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
